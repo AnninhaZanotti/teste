5046ebc16ce9584698acdacb39dba9552748d88a
jgbzera <131875322+AnninhaZanotti@users.noreply.github.com>
2023-08-30 10:04:49 -0300

    Clonando projeto

---
+++ Teste Anna/Assets/Scripts/Menu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject painelMenu, painelCreditos;
+    public AudioSource jukebox;
+    public AudioClip musica, efeito;
+    private void Awake()
+    {
+        painelMenu.SetActive(true);
+        painelCreditos.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        jukebox.Play();
+    }
+
+    // Update is called once per frame
+    public void MudarCreditos()
+    {
+        jukebox.PlayOneShot(efeito);
+        painelMenu.SetActive(false);
+        painelCreditos.SetActive(true);
+    }
+    public void MudarMenu()
+    {
+        jukebox.PlayOneShot(efeito);
+        painelMenu.SetActive(true);
+        painelCreditos.SetActive(false);
+    }
+    public void MinhaPagina()
+    {
+        Application.OpenURL("https://neneca2005.itch.io/");
+    }
+    public void IrParaJogo()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
